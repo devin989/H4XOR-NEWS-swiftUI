25dc18ee2f0cba9c934f2b7de8523201d5424ef2
devin989 <deveenrath@gmail.com>
2021-05-09 17:55:19 +0530

    Initial Commit

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by DEVEEN RATNAYAKE on 2021-05-09.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
